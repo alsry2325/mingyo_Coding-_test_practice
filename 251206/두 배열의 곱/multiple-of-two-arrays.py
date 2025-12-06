4c63e668c14a3b3a83d3d86cf4b66a0a8bff7f47
min gyo jeong <85326632+alsry2325@users.noreply.github.com>
2025-12-06 18:24:17 +0900

    Title: 두 배열의 곱, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251206/두 배열의 곱/multiple-of-two-arrays.py
@@ -0,0 +1,21 @@
+
+# 리스트 컴프리헨션을 이용하여 2차원 리스트 
+row = 3
+cols = 3
+
+matrix1 = []
+matrix2 = []
+for _ in range(row):
+    row_list = list(map(int,input().split()))
+    matrix1.append(row_list)
+input()
+for _ in range(row):
+    row_list = list(map(int,input().split()))
+    matrix2.append(row_list)
+
+for i in range(row):
+    for j in range(cols):
+        results = matrix1[i][j] * matrix2[i][j]
+        print(results,end=" ")
+    print()
+
