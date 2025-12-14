1c2ddf0fb3b992368ba74305a9a53c0f6e5856a7
min gyo jeong <85326632+alsry2325@users.noreply.github.com>
2025-12-14 20:57:10 +0900

    Title: 수의 개수, Time: 169ms, Memory: 23MB, Status: Passed - Codetree

---
+++ 251214/수의 개수/number-of-integers.py
@@ -0,0 +1,38 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+queries = [int(input()) for _ in range(m)]
+
+# Please write your code here.
+def lower_bound(target):
+    left, right = 0, n - 1
+    min_idx = n
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] >= target:
+            min_idx = min(min_idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return min_idx
+
+def upper_bound(target):
+    left, right = 0, n - 1
+    min_idx = n
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] > target:
+            min_idx = min(min_idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return min_idx                      
+
+
+for i in queries:    
+    count = upper_bound(i) - lower_bound(i)
+
+    print(count)
