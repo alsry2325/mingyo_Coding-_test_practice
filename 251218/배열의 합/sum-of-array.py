8bf06e73928b547a7f249437334d237c6b5aeac1
min gyo jeong <85326632+alsry2325@users.noreply.github.com>
2025-12-18 18:00:03 +0900

    Title: 배열의 합, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251218/배열의 합/sum-of-array.py
+++ 251218/배열의 합/sum-of-array.py
@@ -1,10 +1,10 @@
 arr = [
     list(map(int, input().split()))
-    for _ in range(4)
+    for _ in range(4)     #4줄이라 네번 입력 한걸 배열에 넣기
 ]
 
-for i in range(4):
-    ans = 0
-    for j in range(4):
-        ans += arr[i][j]
-    print(ans)
+for i in range(4):  # 4줄이라 4번 실행
+    ans = 0   # 그줄마다 합친 값을 구해야 해서  선언
+    for j in range(4):   # 그줄에 갯수 선언
+        ans += arr[i][j]  # 한줄 값이 들어올때마다  값을 더하기
+    print(ans)  #결과 실행
